d48aecfba356e4fec15d82226652c4cd11e3b12f
jamilWebdesign2024 <jamiluddinjishan@gmail.com>
2025-11-18 09:38:41 +0600

    getlength

--- src/solution.ts
+++ src/solution.ts
@@ -1,25 +1,43 @@
-const formatValue = (value: string | number | boolean)=>{
-    if(typeof value === 'string'){
+// Problem 1:
+
+const formatValue = (value: unknown) => {
+    if (typeof value === 'string') {
         return value.toUpperCase()
     }
-    else if(typeof value === 'number'){
-        return value*10;
+    else if (typeof value === 'number') {
+        return value * 10;
     }
-    else{
-       if(value === true){
+    else {
+        if (value === true) {
             return false;
         }
-        else{
+        else {
             return true;
         }
 
     }
-   
+
     return value;
 }
 
-console.log(formatValue('jamil'));
-console.log(formatValue(10));
-console.log(formatValue(false));
+
+// Problem 2:
+
+const getLength = (input: unknown): unknown => {
+    if (typeof input === 'string') {
+        return input.length
+    }
+    else if (Array.isArray(input)) {
+        return input.length
+    }
+    return input
+}
+
+
+// console.log(getLength('Alhamdulliah'));
+// console.log(getLength(["Apple", "komola", 1, true]));
+
+
+
 
 
